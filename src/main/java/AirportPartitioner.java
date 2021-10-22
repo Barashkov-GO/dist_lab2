1b8ba567d912545ef845b1774844d6b47a194205
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 19:20:00 +0300

    Scripted auto-commit on change (2021-10-22 19:20:00) by gitwatch.sh

--- src/main/java/AirportPartitioner.java
+++ src/main/java/AirportPartitioner.java
@@ -2,5 +2,5 @@ import org.apache.hadoop.mapreduce.Partitioner;
 import java.io.IOException;
 
 public class AirportPartitioner extends <AirportWritableComparable, Text> {
-    public abstract int getPartition(AirportWritableComparable flight, )
+    public abstract int getPartition(AirportWritableComparable flight, Text text, )
         }
