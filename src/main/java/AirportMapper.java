48e88e594d1e1d92bcfb3527d7bb75d8360a0b30
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 21:50:14 +0300

    Scripted auto-commit on change (2021-10-22 21:50:14) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
-    protected void map(IntWritable key, Text value, Context context) throws IOException,
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String[] stringSlices = value.toString().replaceAll("\"", "").split(",");
         if (stringSlices[0] != "Code") {
