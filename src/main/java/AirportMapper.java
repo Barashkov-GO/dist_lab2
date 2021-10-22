1ba1df46052cfde67a6bc8a3f7d6d8dc3a195285
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 17:59:13 +0300

    Scripted auto-commit on change (2021-10-22 17:59:13) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -2,4 +2,4 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 
-public class WordMapper extends Mapper<Text, IntWritable> {
+public class WordMapper extends Mapper<IntWritable, Text, IntWritable> {
