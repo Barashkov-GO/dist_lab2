8e106c69f8490dba11cd669612f9282a38dd4c9b
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 21:31:08 +0300

    Scripted auto-commit on change (2021-10-22 21:31:08) by gitwatch.sh

--- src/main/java/AirlineMapper.java
+++ src/main/java/AirlineMapper.java
@@ -10,7 +10,7 @@ public class AirlineMapper extends Mapper<IntWritable, Text, AirportWritableComp
             InterruptedException {
         String[] stringSlices = value.toString().split(",");
         int airportId = Integer.parseInt(stringSlices[14]);
-        int arrivalDelay = Float.parseFloat(stringSlices[18] != "" ? stringSlices[18] : 0);
+        float arrivalDelay = Float.parseFloat(stringSlices[18] != "" ? stringSlices[18] : "0");
         if (arrivalDelay != 0.0f) {
             context.write(new AirportWritableComparable(airportId, 1), new Text(String.valueOf(arrivalDelay)));
         }
