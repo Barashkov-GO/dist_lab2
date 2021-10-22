130716a76258cac0ba10e3fc139d61a62b0ada5f
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 21:13:35 +0300

    Scripted auto-commit on change (2021-10-22 21:13:35) by gitwatch.sh

--- src/main/java/FlightsApp.java
+++ src/main/java/FlightsApp.java
@@ -14,8 +14,8 @@ public class App {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, AirlineMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setPartitionerClass(TextPair.FirstPartitioner.class);
-        job.setGroupingComparatorClass(TextPair.FirstComparator.class);
+        job.setPartitionerClass(AirportPartitioner.class);
+        job.setGroupingComparatorClass(AirportComparator.class);
         job.setReducerClass(JoinReducer.class);
         job.setMapOutputKeyClass(TextPair.class);
         job.setOutputKeyClass(Text.class);
