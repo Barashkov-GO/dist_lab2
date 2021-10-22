de6f09136205e72f0cbe4f820f737161a849cc41
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 22:05:32 +0300

    Scripted auto-commit on change (2021-10-22 22:05:32) by gitwatch.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-hadoop -fs rm -r output && mvn package && export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar && hadoop FlightsApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output4 && hadoop fs -copyToLocal output4
+hadoop fs rm -r output && mvn package && export HADOOP_CLASSPATH=target/hadoop-examples-1.0-SNAPSHOT.jar && hadoop FlightsApp 664600583_T_ONTIME_sample.csv L_AIRPORT_ID.csv output4 && hadoop fs -copyToLocal output4
