01d7cb04711ad5cf73819668614727ed80d5b5cb
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 18:41:45 +0300

    Scripted auto-commit on change (2021-10-22 18:41:45) by gitwatch.sh

--- src/main/java/AirlineWritableComparable.java
+++ src/main/java/AirlineWritableComparable.java
@@ -15,18 +15,4 @@ public class AirlineWritable implements WritableComparable {
         this->destinationAirportId = in.readInt();
         this->arrivalDelay = in.readInt();
     }
-
-    @Override
-    public int compareTo(AirportWritableComparable otherFlight) {
-        if (this->airportId > otherFlight.airportId){
-            return 1;
-        } else if (this->airportId < otherFlight.airportId) {
-            return -1;
-        } else if (this->indicator && !otherFlight.indicator) {
-            return 1;
-        } else if (!this->indicator && otherFlight.indicator) {
-            return -1;
-        }
-        return 0;
-    }
 }
