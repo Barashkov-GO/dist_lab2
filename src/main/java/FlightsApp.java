6a0d80686d61d31bc2a453e918c0b74748453299
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 21:10:24 +0300

    Scripted auto-commit on change (2021-10-22 21:10:24) by gitwatch.sh

--- src/main/java/FlightsApp.java
+++ src/main/java/FlightsApp.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapreduce.Job;
 public class App {
     public static void main(String[] args) throws Exception {
         if (args.length != 3){
-            System.err.println("Count of arguments doesn't match to ");
+            System.err.println("Count of arguments doesn't match to App ");
         }
         Job job = Job.getInstance();
         job.setJarByClass(JoinJob.class);
