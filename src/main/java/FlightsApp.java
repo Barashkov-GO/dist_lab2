bf7c6e8b272dec10677596db39a66af605e10191
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 21:12:13 +0300

    Scripted auto-commit on change (2021-10-22 21:12:13) by gitwatch.sh

--- src/main/java/FlightsApp.java
+++ src/main/java/FlightsApp.java
@@ -10,7 +10,7 @@ public class App {
         }
         Job job = Job.getInstance();
         job.setJarByClass(App.class);
-        job.setJobName("JoinJob sort");
+        job.setJobName("Airports reduce side join");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
