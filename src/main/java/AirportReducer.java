731b6033a817a44b3df99b0dbea08d7ce570ee02
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 19:54:30 +0300

    Scripted auto-commit on change (2021-10-22 19:54:30) by gitwatch.sh

--- src/main/java/AirportReducer.java
+++ src/main/java/AirportReducer.java
@@ -21,6 +21,6 @@ public class AirportReducer extends Reducer<AirportWritableComparable, Text, Tex
             sumTime += newDelay;
             count += 1;
         }
-        context.write()
+        context.write(new Text())
     }
 }
