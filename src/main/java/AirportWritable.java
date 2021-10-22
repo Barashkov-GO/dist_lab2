cd9fb690d4780373a08c62b93812026173c677a0
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 18:19:43 +0300

    Scripted auto-commit on change (2021-10-22 18:19:43) by gitwatch.sh

--- src/main/java/AirportWritable.java
+++ src/main/java/AirportWritable.java
@@ -1,8 +1,11 @@
 import org.apache.hadoop.io.Writable;
+import java.io.IOException;
+
 
 public class AirportWritable implements Writable {
     private int airportId;
-    public void readFields()
 
-
+    public void readFields(DataInput in) throws IOException  {
+        this->airportId = in.readint();
+    }
 }
