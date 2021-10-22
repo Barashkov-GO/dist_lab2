3254755abbc7715dcca7acff581fa61351ead6da
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 19:37:18 +0300

    Scripted auto-commit on change (2021-10-22 19:37:18) by gitwatch.sh

---
+++ src/main/java/AirportComparator.java
@@ -0,0 +1,5 @@
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.Comp;
+
+public class AirportsGroupingComparatorClass extends WritableComparator {
