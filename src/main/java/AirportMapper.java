873a29fea3c2a215f97b614b6137e7a9c0f50691
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-22 18:01:24 +0300

    Scripted auto-commit on change (2021-10-22 18:01:24) by gitwatch.sh

--- src/main/java/AirportMapper.java
+++ src/main/java/AirportMapper.java
@@ -6,10 +6,7 @@ public class WordMapper extends Mapper<IntWritable, Text, AirportWritable, Text>
     @Override
     protected void map(IntWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String[] words = value.toString().replaceAll("[\\n\\r\\t.,!?*\"'\\[\\]{}~$#+=/()]", "")
-                .replaceAll("  ", " ")
-                .toLowerCase()
-                .split(" ");
+        String[] words = value.toString();
         for (String word : words) {
             if (!word.isEmpty()) {
                 context.write(new Text(String.valueOf(word.length())), new IntWritable(1));
