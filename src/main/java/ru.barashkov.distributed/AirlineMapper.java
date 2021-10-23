392957c8cc37a1c3d0606c5712560a1c3f19bb31
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-23 13:42:36 +0300

    Scripted auto-commit on change (2021-10-23 13:42:36) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed/AirlineMapper.java
+++ src/main/java/ru.barashkov.distributed/AirlineMapper.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 
 
 public class AirlineMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
-    private static final int AIRPORT_ID_ID = 14;
-    private static final int FLIGHT_DELAY_ID = 18;
+    private static final int AIRPORT_ID_POS = 14;
+    private static final int FLIGHT_DELAY_POS = 18;
     private static final String SEPARATOR = ",";
-    private static final String CHECK = "\"DEST_AIRPORT_ID\"";
+    private static final String CHECK_WRONG = "\"DEST_AIRPORT_ID\"";
     private static final int INDICATOR = 1;
 
     @Override
@@ -18,9 +18,9 @@ public class AirlineMapper extends Mapper<LongWritable, Text, AirportWritableCom
             InterruptedException {
         String[] stringSlices = value.toString().split(SEPARATOR);
 
-        if (!stringSlices[AIRPORT_ID_ID].equals(CHECK)) {
-            int airportId = Integer.parseInt(stringSlices[AIRPORT_ID_ID]);
-            String flightDelayStr = stringSlices[FLIGHT_DELAY_ID];
+        if (!stringSlices[AIRPORT_ID_POS].equals(CHECK_WRONG)) {
+            int airportId = Integer.parseInt(stringSlices[AIRPORT_ID_POS]);
+            String flightDelayStr = stringSlices[FLIGHT_DELAY_POS];
             if (!flightDelayStr.equals("") && Float.parseFloat(flightDelayStr) != 0.0f){
                 context.write(new AirportWritableComparable(airportId, INDICATOR), new Text(flightDelayStr));
             }
