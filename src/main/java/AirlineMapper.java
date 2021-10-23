e64c88c3f280f543cf16f3d179aa5022846db1e9
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-23 12:10:33 +0300

    Scripted auto-commit on change (2021-10-23 12:10:33) by gitwatch.sh

--- src/main/java/AirlineMapper.java
+++ src/main/java/AirlineMapper.java
@@ -12,7 +12,7 @@ public class AirlineMapper extends Mapper<LongWritable, Text, AirportWritableCom
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String[] stringSlices = value.toString().split(",");
-        if (!stringSlices[airportId].equals("\"DEST_AIRPORT_ID\"") && key.get() > 0) {
+        if (!stringSlices[AIRPORT_ID_ID].equals("\"DEST_AIRPORT_ID\"") && key.get() > 0) {
             int airportId = Integer.parseInt(stringSlices[AIRPORT_ID_ID]);
             String flightDelayStr = stringSlices[FLIGHT_DELAY_ID];
             if (!flightDelayStr.equals("") && Float.parseFloat(flightDelayStr) != 0.0f){
