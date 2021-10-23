4015a6873b0836ac0963885933199e74b4fe57e7
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-23 12:36:35 +0300

    Scripted auto-commit on change (2021-10-23 12:36:35) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed/AirportMapper.java
+++ src/main/java/ru.barashkov.distributed/AirportMapper.java
@@ -10,13 +10,15 @@ public class AirportMapper extends Mapper<LongWritable, Text, AirportWritableCom
     private static final String SEPARATOR = ",";
     private static final String TO_DELETE = "\"";
     private static final String CHECK = "Code";
+    private static final int SPLIT_TO = 2;
+
 
     private static final int AIRPORT_ID_ID = 14;
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String[] stringSlices = value.toString().replaceAll(TO_DELETE, "").split(SEPARATOR, 2);
+        String[] stringSlices = value.toString().replaceAll(TO_DELETE, "").split(SEPARATOR, SPLIT_TO);
         if (!stringSlices[0].equals(CHECK)) {
             context.write(new AirportWritableComparable(Integer.parseInt(stringSlices[0]), 0), new Text(stringSlices[1]));
         }
