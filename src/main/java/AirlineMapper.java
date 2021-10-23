03028803d56f6025910b5a20bb6cf657fa6561db
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-23 11:53:24 +0300

    Scripted auto-commit on change (2021-10-23 11:53:24) by gitwatch.sh

--- src/main/java/AirlineMapper.java
+++ src/main/java/AirlineMapper.java
@@ -5,14 +5,17 @@ import java.io.IOException;
 
 
 public class AirlineMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    private static const int airportIdId = 14;
+    private static const int flightDelayId = 18;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         String[] stringSlices = value.toString().split(",");
-        if (!stringSlices[14].equals("\"DEST_AIRPORT_ID\"") && key.get() > 0) {
-            int airportId = Integer.parseInt(stringSlices[14]);
-            if (!stringSlices[18].equals("") && Float.parseFloat(stringSlices[18]) != 0.0f){
-                context.write(new AirportWritableComparable(airportId, 1), new Text(stringSlices[18]));
+        if (!stringSlices[airportId].equals("\"DEST_AIRPORT_ID\"") && key.get() > 0) {
+            int airportId = Integer.parseInt(stringSlices[airportIdId]);
+            String flightDelayStr = stringSlices[flightDelayId];
+            if (!flightDelayStr.equals("") && Float.parseFloat(flightDelayStr) != 0.0f){
+                context.write(new AirportWritableComparable(airportId, 1), new Text(flightDelayStr));
             }
         }
     }
