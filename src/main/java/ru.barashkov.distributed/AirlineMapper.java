852e01b7a41fe48ad9a58c8bdc171a1ebbc123ab
Barashkov-GO <gbarashkov2001@gmail.com>
2021-10-23 12:39:49 +0300

    Scripted auto-commit on change (2021-10-23 12:39:49) by gitwatch.sh

--- src/main/java/ru.barashkov.distributed/AirlineMapper.java
+++ src/main/java/ru.barashkov.distributed/AirlineMapper.java
@@ -9,15 +9,18 @@ import java.io.IOException;
 public class AirlineMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     private static final int AIRPORT_ID_ID = 14;
     private static final int FLIGHT_DELAY_ID = 18;
+    private static final String SEPARATOR = ",";
+    private static final String CHECK = "\"DEST_AIRPORT_ID\"";
+    private static final int INDICATOR = 1;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String[] stringSlices = value.toString().split(",");
-        if (!stringSlices[AIRPORT_ID_ID].equals("\"DEST_AIRPORT_ID\"") && key.get() > 0) {
+        String[] stringSlices = value.toString().split(SEPARATOR);
+        if (!stringSlices[AIRPORT_ID_ID].equals(CHECK) && key.get() > 0) {
             int airportId = Integer.parseInt(stringSlices[AIRPORT_ID_ID]);
             String flightDelayStr = stringSlices[FLIGHT_DELAY_ID];
             if (!flightDelayStr.equals("") && Float.parseFloat(flightDelayStr) != 0.0f){
-                context.write(new AirportWritableComparable(airportId, 1), new Text(flightDelayStr));
+                context.write(new AirportWritableComparable(airportId, INDICATOR), new Text(flightDelayStr));
             }
         }
     }
